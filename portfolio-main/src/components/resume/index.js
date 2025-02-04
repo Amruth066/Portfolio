454f63d033fa869c5d10e80b652143023c19d55b
Amruth <amruthsaimanchu@gmail.com>
2025-02-04 18:24:30 +0530

    feat:modified the resume component

--- portfolio-main/src/components/resume/index.js
+++ portfolio-main/src/components/resume/index.js
@@ -1,11 +1,21 @@
-// Resume.js
 import React from 'react';
+import resumePDF from '../../assets/resume.pdf'; 
+import './index.scss';
 
 const Resume = () => {
     return (
-        <div>
-            <h1>My Resume</h1>
-            {/* You can add a download link or an embedded resume */}
+        <div className="resume-container">
+            <iframe 
+                src={resumePDF} 
+                className="resume-iframe"
+                title="Resume Preview"
+            ></iframe>
+
+            <div className="button-container">
+                <a href={resumePDF} download="My_Resume.pdf">
+                    <button className="action-btn download-btn">Download Resume</button>
+                </a>
+            </div>
         </div>
     );
 };
