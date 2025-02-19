dd875449d9ae90a7d1b17481247c649e78c1fdef
Amruth <amruthsaimanchu@gmail.com>
2025-02-19 17:52:35 +0530

    added updated resume

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -21,9 +21,6 @@ const SideBar = () => {
                 <NavLink exact="true" activeclassname="active" className="contact-link" to={'/contact'}>
                     <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
                 </NavLink>
-                {/* <NavLink exact="true" activeclassname="active" className="projects-link" to={'/projects'}>
-                    <FontAwesomeIcon icon={faFolder} color='#4d4d4e' />
-                </NavLink> */}
                 <NavLink exact="true" activeclassname="active" className="resume-link" to={'/resume'}>
                     <FontAwesomeIcon icon={faFileAlt} color='#4d4d4e' />
                 </NavLink>
